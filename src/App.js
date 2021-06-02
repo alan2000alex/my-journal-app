6b9e6c28559da338e23634f0c04f3205109729d9
Alan <alan2000alex@gmail.com>
2021-06-02 17:55:10 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Entries from './Entries';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    entries: [
+
+    ]
+  }
+
+  removeEntry = index => {
+    const { entries } = this.state
+
+    this.setState({
+      entries: entries.filter ((entry, i) => {
+        return i != index
+      })
+    })
+  }
+
+  handleSubmit = entry => {
+    this.setState({ entries: [...this.state.entries, entry]})
+  }
+
+  render () {
+    const { entries } = this.state;
+    
+    return (
+      <div className="App">
+        <h1>My Journal App</h1>
+        <Entries entryData={entries} removeEntry={this.removeEntry}/>
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
